2e46ac4b13b4b2dd0b2f0d862698a2f74a6f54fe
Night-Time1809 <chanoknun.wora@gmail.com>
2022-11-10 10:08:01 +0000

    101165-3

--- app.py
+++ app.py
@@ -25,28 +25,63 @@ def search_picture(poster_path, api_key="8265bd1679663a7ea12ac168da84d2e8"):
     
     return fetch_pic
 
-df_display = pd.read_csv("")
-
+df_display = pd.read_csv("display_data.csv", index_col=0)
+df_sorted_vote_avg = df_display[df_display.sort_values(by="vote_avg", ascending=False)["vote_count"] >= 15000]
+df_sorted_vote_count = df_display.sort_values(by="vote_count", ascending=False)
 
 st.title("🎬 Movie Recommendation System")
 st.header("Welcome, Many of movies to discover. Explore now. 🔍")
+
+selected_movie_name = st.selectbox("Search for a movie...", df_display["title"].values)
 
 st.subheader("What's Popular")
 
 tab1, tab2 = st.tabs(["Most vote scores", "Most votes"])
 
 with tab1:
-    col1, col2, col3 = st.columns(3)
-    columns = [col1, col2, col3]
-    for col in columns:
-        with col:
-            st.image("https://static.streamlit.io/examples/cat.jpg")
-            st.text("Movie name")
+    with st.expander(""):
+        col1, col2, col3 = st.columns(3)
+        columns = [col1, col2, col3]
+        count = 0
+        button1 = st.button("Show more..")
+
+        if button1:
+            num = 9
+        else:
+            num = 3
+        
+        for i in range(num):
+            with columns[count]:
+                id_ = df_sorted_vote_avg["id"].iloc[i]
+                poster_path = search_data(id=id_, word="poster_path")
+
+                st.image(search_picture(poster_path))
+                st.text(df_sorted_vote_avg["title"].iloc[i])
+
+                count += 1
+                if (i+1)%3 == 0:
+                    count = 0
 
 with tab2:
-    col1, col2, col3 = st.columns(3)
-    columns = [col1, col2, col3]
-    for col in columns:
-        with col:
-            st.image("https://static.streamlit.io/examples/dog.jpg")
-            st.text("Movie name")
+    with st.expander(""):
+        col1, col2, col3 = st.columns(3)
+        columns = [col1, col2, col3]
+        count = 0
+        button2 = st.button("Show more...")
+
+        if button2:
+            num = 9
+        else:
+            num = 3
+
+        for i in range(num):
+            with columns[count]:
+                id_ = df_sorted_vote_count["id"].iloc[i]
+                poster_path = search_data(id=id_, word="poster_path")
+
+                st.image(search_picture(poster_path))
+                st.text(df_sorted_vote_count["title"].iloc[i])
+
+                count += 1
+                if (i+1)%3 == 0:
+                    count = 0

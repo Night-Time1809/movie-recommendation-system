f10a22bfb393e60be8ebcd6fd758033bc9cededa
Night-Time1809 <chanoknun.wora@gmail.com>
2022-11-10 20:01:41 +0000

    111165-1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import requests
 import pandas as pd
+from datetime import datetime, time
 
 def search_data(id, word, api_key="8265bd1679663a7ea12ac168da84d2e8"):
     try:
@@ -38,10 +39,54 @@ st.subheader("What's Popular")
 
 tab1, tab2 = st.tabs(["Most vote scores", "Most votes"])
 
+# if st.button("Filter"):
+    #     col_1, col_2, col_3 = st.columns(3)
+    #     with col_1:
+    #         st.write("Streaming platform:")
+    #         netflix = st.checkbox('Netflix')
+    #         disney_plus = st.checkbox('Disney+')
+    #         amazon_prime = st.checkbox('Amazon Prime')
+    #         hulu = st.checkbox('Hulu')
+    #     with col_2:
+    #         st.write("Vote score range:")
+    #         values = st.slider("Select a range of vote score", 0.0, 10.0)
+    # else:
+    #     st.write("no")
+
 with tab1:
     with st.expander(""):
-        col1, col2, col3 = st.columns(3)
-        columns = [col1, col2, col3]
+        if st.button("Filter"):
+            col = st.columns(2)
+            with col[0]:
+                st.write("Streaming platform:")
+                netflix = st.checkbox('Netflix', value=True)
+                disney_plus = st.checkbox('Disney+', value=True)
+                amazon_prime = st.checkbox('Amazon Prime', value=True)
+                hulu = st.checkbox('Hulu' , value=True)
+            with col[1]:
+                st.write("Year:")
+                values = st.slider("Select a range of year", 1995, 2020, (1995, 2020))
+
+            st.write(" ")
+            col = st.columns(2)
+            with col[0]:
+                st.write("Vote score:")
+                values = st.slider("Select a range of vote score", 0.0, 10.0, (0.0, 10.0))
+            with col[1]:
+                st.write("Vote count:")
+                values = st.slider("Select a range of vote count", 0.0, 10.0, (0.0, 10.0))
+        # else:
+        #     st.write("no")
+        # st.write("**Filter**")
+        # col = st.columns(3)
+        # with col[0]:
+        #     st.write("Streaming platform:")
+        #     netflix = st.checkbox('Netflix')
+        #     disney_plus = st.checkbox('Disney+')
+        #     amazon_prime = st.checkbox('Amazon Prime')
+        #     hulu = st.checkbox('Hulu')
+
+        col = st.columns(3)
         count = 0
         button1 = st.button("Show more..")
 
@@ -51,7 +96,7 @@ with tab1:
             num = 3
         
         for i in range(num):
-            with columns[count]:
+            with col[count]:
                 id_ = df_sorted_vote_avg["id"].iloc[i]
                 poster_path = search_data(id=id_, word="poster_path")
 
@@ -64,8 +109,7 @@ with tab1:
 
 with tab2:
     with st.expander(""):
-        col1, col2, col3 = st.columns(3)
-        columns = [col1, col2, col3]
+        col = st.columns(3)
         count = 0
         button2 = st.button("Show more...")
 
@@ -75,7 +119,7 @@ with tab2:
             num = 3
 
         for i in range(num):
-            with columns[count]:
+            with col[count]:
                 id_ = df_sorted_vote_count["id"].iloc[i]
                 poster_path = search_data(id=id_, word="poster_path")
 
@@ -85,3 +129,20 @@ with tab2:
                 count += 1
                 if (i+1)%3 == 0:
                     count = 0
+
+# st.subheader('Datetime slider')
+
+# start_time = st.slider(
+#      "When do you start?",
+#      value=datetime(2020),
+#      format="YY")
+# st.write("Start time:", start_time)
+
+# st.subheader('Range time slider')
+
+# appointment = st.slider(
+#      "Schedule your appointment:",
+#      value=(time(11, 30), time(12, 45)))
+# st.write("You're scheduled for:", appointment)
+st.write("Vote score range:")
+values = st.slider("Select a range of vote score", 2000, 2020, (2000, 2020))
